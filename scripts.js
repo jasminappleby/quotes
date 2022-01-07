a062ad48d7f3bf254e3139da400c7ce7ac4baa61
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-07 16:14:57 +0000

    added dark mode

--- scripts.js
+++ scripts.js
@@ -18,3 +18,17 @@ async function generateJoke() {
 
   jokeEl.innerHTML = data.joke
 }
+
+const toggle = document.querySelector("#toggle");
+toggle.addEventListener("click", modeSwitch);
+
+let isLight = true;
+
+function modeSwitch() {
+  isLight = !isLight;
+  let root = document.body;
+  
+  isLight ? toggle.innerText = "🌞" : toggle.innerText = "🌚";
+  
+  root.classList.toggle("darkMode");
+}
